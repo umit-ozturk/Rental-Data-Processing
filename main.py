ea28c3704c5d5bb36a15c0e1a7e4c2fd53dcb1e9
Umit Ozturk <umit.ozturk@deepensive.com>
2021-09-09 20:41:25 +0300

    Filter the rental data by date range completed

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from utils import (
     sort_by_current_rent,
     filter_by_lease_years,
     get_tenant_names_and_counts,
+    list_rentals_by_date_range,
 )
 from constant import CSV_PATH
+from datetime import datetime
 import argparse
 import csv
 
@@ -32,15 +34,34 @@ def parse_arguments():
         type=int,
         help="Pass to system the desired lease year. Default is 25.",
     )
+
+    parser.add_argument(
+        "--start-date",
+        default=datetime.strptime("01 Jun 1999", "%d %b %Y"),
+        type=lambda d: datetime.strptime(d, "%d %b %Y"),
+        help="Default is ascending. If you want to sort "
+        "descending send the parameter as True",
+    )
+
+    parser.add_argument(
+        "--end-date",
+        default=datetime.strptime("31 Aug 2007", "%d %b %Y"),
+        type=lambda d: datetime.strptime(d, "%d %b %Y"),
+        help="Default is ascending. If you want to sort "
+        "descending send the parameter as True",
+    )
+
     return parser.parse_args()
 
 
 def main():
     args = parse_arguments()
-    sort_type, item_count, lease_year = (
+    sort_type, item_count, lease_year, start_date, end_date = (
         args.sorting_type,
         args.item,
         args.lease_year,
+        args.start_date,
+        args.end_date,
     )
 
     reader = csv.DictReader(open(CSV_PATH, "r"))
@@ -54,6 +75,17 @@ def main():
     tenant_summary_data = get_tenant_names_and_counts(csv_data)
     print(tenant_summary_data)
     print("*" * 50)
+    print(start_date)
+    filtered_rents_by_date_range = list_rentals_by_date_range(
+        csv_data, start_date, end_date
+    )
+    print(
+        {
+            "start_date": datetime.strftime(start_date, "%d/%m/%y"),
+            "end_date": datetime.strftime(end_date, "%d/%m/%y"),
+            "data": filtered_rents_by_date_range,
+        }
+    )
 
 
 main()
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from datetimerange import DateTimeRange
 import itertools
 import operator
 
@@ -42,3 +43,34 @@ def get_tenant_names_and_counts(data):
         else:
             o[i] = len(list(g))
     return [{"Tenant Name": k, "Count": v} for k, v in o.items()]
+
+
+def check_datetime_in_range(start, end, date):
+    """
+    Returns related date is in the range or not.
+    :param start: Datetime object, Start Date
+    :param end: Datetime object, End Date
+    :param date: Datetime object, Date which is queried
+    :return: Boolean
+    """
+    return date in DateTimeRange(start, end)
+
+
+def list_rentals_by_date_range(data, start_date, end_date):
+    """
+    Lists data for rentals by a date range.
+    :param data: Data which is coming from reader.
+    :param start_date: Datetime object, Start Date
+    :param end_date: Datetime object, End Date
+    :return: List of rental record between desired dates, Start date and
+    end date.
+    """
+    filtered_data = list(
+        filter(
+            lambda x: check_datetime_in_range(
+                start_date, end_date, x["Lease Start Date"]
+            ),
+            data,
+        )
+    )
+    return filtered_data
