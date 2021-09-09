ded562a405f695b5c87487c1bcd1d6b02907b5a9
Umit Ozturk <umit.ozturk@deepensive.com>
2021-09-09 18:19:25 +0300

    Argparser functionality was appended

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from utils import sort_by_current_rent
 from constant import CSV_PATH
+import argparse
 import csv
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Process and output the data created for tenants."
+    )
+    parser.add_argument(
+        "--sorting-type",
+        default=False,
+        type=lambda x: (str(x).lower() == "true"),
+        help="Default is ascending. If you want to sort "
+        "descending send the parameter as True",
+    )
+    parser.add_argument(
+        "--item",
+        default=5,
+        type=int,
+        help="Returning item count. Default is 0. ",
+    )
+    return parser.parse_args()
+
+
 def main():
-    sort_type = True  # Argparse
-    n = 5  # Argparse
+    args = parse_arguments()
+    sort_type, item_count = args.sorting_type, args.item
+
     reader = csv.DictReader(open(CSV_PATH, "r"))
-    sorted_rents = sort_by_current_rent(list(reader), sort_type, n)
+    sorted_rents = sort_by_current_rent(list(reader), sort_type, item_count)
     print(sorted_rents)  # Print to console
 
 
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 def sort_by_current_rent(data, sort_type=False, n=5):
     """
-
-    :param data:
-    :param sort_type: Boolean (asc --> False, desc --> True)
-    :param n:
-    :return:
+    Sorts the data by current rent.
+    :param data: Data which is coming from reader.
+    :param sort_type: Boolean, Sorting Type.
+    :param n: Int, Item count.
+    :return: Sorted data which is contain current rents.
     """
     return sorted(
         data, key=lambda x: float(x["Current Rent"]), reverse=sort_type
